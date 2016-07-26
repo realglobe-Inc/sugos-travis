5f2a1447338de1590074dbe7b2ad5dcdb05f8858
FujiHaruka <fujiharuka@realglobe.jp>
2016-07-26 19:40:57 +0900

    .npmignoreのファイルを無視する

---
+++ test/has_library_diff_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for hasLibraryDiff.
+ * Runs with mocha.
+ */
+'use strict'
+
+const hasLibraryDiff = require('../lib/util/has_library_diff.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('has-library-diff', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Has library diff', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
---
+++ test/is_empty_commit_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for isEmptyCommit.
+ * Runs with mocha.
+ */
+'use strict'
+
+const isEmptyCommit = require('../lib/util/is_empty_commit.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('is-empty-commit', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Is empty commit', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
