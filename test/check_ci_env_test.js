c1cafe05f3c675619e400e452ec6c0cf7054b8d7
FujiHaruka <fujiharuka@realglobe.jp>
2016-07-27 11:00:23 +0900

    Run build

---
+++ test/check_ci_env_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for checkCiEnv.
+ * Runs with mocha.
+ */
+'use strict'
+
+const checkCiEnv = require('../lib/util/check_ci_env.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('check-ci-env', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Check ci env', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
