d098b2817bdd2ff9c2532307d704a9ac2a0ef2da
FujiHaruka <fujiharuka@realglobe.jp>
2016-07-27 11:53:18 +0900

    .nmprcを/home/fujiharukaに作る

--- bin/create_npmrc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-
-echo "registry = https://realglobe.artifactoryonline.com/realglobe/api/npm/npm-virtual" >> $HOME/.npmrc
-curl -u${NPM_USER}:${NPM_PASSWORD} "https://realglobe.artifactoryonline.com/realglobe/api/npm/auth" >> $HOME/.npmrc
-cat $HOME/.npmrc
